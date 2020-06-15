e012219959f291a2c262a790661dfaea4bc326a9
MarcosPBrandao <brandaomarcosp@gmail.com>
2020-06-15 17:30:04 -0300

    quinto commit

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
 const express = require('express');
-const { request } = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get('/projects', (request, response) => {
+    //console.log('Passei por get');
+    //const { title, owner } = request.query;  
+    const query = request.query;  
+    console.log(query);
+    //console.log(title);
+    //console.log(owner);
     return response.json([
-        'Projeto 1',
+        'Projeto 8',
         'Projeto 2'
     ]);
 })
 
 app.post('/projects', (request, response) => {
+    const body = request.body;
+    //console.log('Passei por post');
+    console.log(body);
     return response.json([
-        'Projeto 1',
+        'Projeto 10',
         'Projeto 2',
         'Projeto 3'
     ]);
 })
 app.put('/projects/:id', (request, response) => {
+    const params = request.params;
+    console.log(params);
+    //console.log('Passei por put');
     return response.json([
-        'Projeto 4',
+        'Projeto 0',
         'Projeto 2',
         'Projeto 3'
     ]);
 })
 app.delete('/projects/:id', (request, response) => {
+    //console.log('Passei por delete');
     return response.json([
         'Projeto 2',
         'Projeto 3'
