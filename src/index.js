53963cec2336bdc62d2b00cea9adfb6a97fd34a9
MarcosPBrandao <brandaomarcosp@gmail.com>
2020-06-15 15:15:45 -0300

    quarto commit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,34 @@ const { request } = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({message:'Hello Word com nodemon'});
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2'
+    ]);
 })
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+})
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+})
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+})
+
 
 app.listen(3333, () => {
     console.log('😍😍😍Backed started')
