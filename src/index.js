8696405e65c0026ca6374dd62e110c3e5b42ff77
MarcosPBrandao <brandaomarcosp@gmail.com>
2020-06-15 14:49:44 -0300

    terceiro commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const { request } = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({message:'Hello Word'});
+    return response.json({message:'Hello Word com nodemon'});
 })
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('😍😍😍Backed started')
+});
